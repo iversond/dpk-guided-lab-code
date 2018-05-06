9dcbff9eb8859ea30e1f0ac32758e3e8068cad80
Dan Iverson <dan@daniverson.com>
2018-05-06 09:16:11 -0500

    Update Header path

--- production/modules/dpk_lab/manifests/io_web.pp
+++ production/modules/dpk_lab/manifests/io_web.pp
@@ -7,12 +7,24 @@ class dpk_lab::io_web {
     $site_list.each | $site_name, $site_info | {
 
       $ps_cfg_home = $pia_domain_info['ps_cfg_home_dir']
-      $site_path = "${ps_cfg_home}/webserv/${domain_name}/applications/peoplesoft/PORTAL.war/${site_name}"
+      $portal_path = "${ps_cfg_home}/webserv/${domain_name}/applications/peoplesoft/PORTAL.war"
 
-      file { "${domain_name}-${site_name}-pia-logo":
+      file { "${domain_name}-${site_name-pia-logo}":
         ensure => present,
-        path   => "${site_path}/images/Header.png",
+        path   => "${portal_path}/${site_name}/images/Header.png",
         source => "puppet:///modules/dpk_lab/dpk-lab-logo-${::app}.png",
+      }
+
+      $text_properties = $site_info['text_properties']
+      $text_properties.each | $key, $value | {
+        ini_setting { "${domain_name}-${site_name}-text-${key}":
+          ensure            => present,
+          path              => "${portal_path}/WEB-INF/psftdocs/${site_name}/text.properties",
+          setting           => $key,
+          value             => $value,
+          key_val_separator => '=',
+          section           => '',
+        }
       }
     } # end-site
 
